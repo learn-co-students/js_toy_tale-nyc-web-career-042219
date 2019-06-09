bfe40cdbff285cc6840d16d330fcbd1147962be3
Kirbyasdf <kirbyAugustus@gmail.com>
2019-06-09 02:50:57 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
+
+ // #     88                      ,ad8888ba,                     88        88
+ // #     88                     d8"'    `"8b                    88        88
+ // #     88                    d8'                              88        88
+ // #     88                    88                               88        88
+ // #     88                    88                               88        88
+ // #     88                    Y8,                              88        88
+ // #     88                     Y8a.    .a8P                    Y8a.    .a8P
+ // #     88                      `"Y8888Y"'                      `"Y8888Y"'
+ // #
+ // #
+ // #
+ // #
+ // #     						               ...----....
+ // #                         ..-:"''         ''"-..
+ // #                      .-'                      '-.
+ // #                    .'              .     .       '.
+ // #                  .'   .          .    .      .    .''.
+ // #                .'  .    .       .   .   .     .   . ..:.
+ // #              .' .   . .  .       .   .   ..  .   . ....::.
+ // #             ..   .   .      .  .    .     .  ..  . ....:IA.
+ // #            .:  .   .    .    .  .  .    .. .  .. .. ....:IA.
+ // #           .: .   .   ..   .    .     . . .. . ... ....:.:VHA.
+ // #           '..  .  .. .   .       .  . .. . .. . .....:.::IHHB.
+ // #          .:. .  . .  . .   .  .  . . . ...:.:... .......:HIHMM.
+ // #         .:.... .   . ."::"'.. .   .  . .:.:.:II;,. .. ..:IHIMMA
+ // #         ':.:..  ..::IHHHHHI::. . .  ...:.::::.,,,. . ....VIMMHM
+ // #        .:::I. .AHHHHHHHHHHAI::. .:...,:IIHHHHHHMMMHHL:. . VMMMM
+ // #       .:.:V.:IVHHHHHHHMHMHHH::..:" .:HIHHHHHHHHHHHHHMHHA. .VMMM.
+ // #       :..V.:IVHHHHHMMHHHHHHHB... . .:VPHHMHHHMMHHHHHHHHHAI.:VMMI
+ // #       ::V..:VIHHHHHHMMMHHHHHH. .   .I":IIMHHMMHHHHHHHHHHHAPI:WMM
+ // #       ::". .:.HHHHHHHHMMHHHHHI.  . .:..I:MHMMHHHHHHHHHMHV:':H:WM
+ // #       :: . :.::IIHHHHHHMMHHHHV  .ABA.:.:IMHMHMMMHMHHHHV:'. .IHWW
+ // #       '.  ..:..:.:IHHHHHMMHV" .AVMHMA.:.'VHMMMMHHHHHV:' .  :IHWV
+ // #        :.  .:...:".:.:TPP"   .AVMMHMMA.:. "VMMHHHP.:... .. :IVAI
+ // #       .:.   '... .:"'   .   ..HMMMHMMMA::. ."VHHI:::....  .:IHW'
+ // #       ...  .  . ..:IIPPIH: ..HMMMI.MMMV:I:.  .:ILLH:.. ...:I:IM
+ // #     : .   .'"' .:.V". .. .  :HMMM:IMMMI::I. ..:HHIIPPHI::'.P:HM.
+ // #     :.  .  .  .. ..:.. .    :AMMM IMMMM..:...:IV":T::I::.".:IHIMA
+ // #     'V:.. .. . .. .  .  .   'VMMV..VMMV :....:V:.:..:....::IHHHMH
+ // #       "IHH:.II:.. .:. .  . . . " :HB"" . . ..PI:.::.:::..:IHHMMV"
+ // #        :IP""HHII:.  .  .    . . .'V:. . . ..:IH:.:.::IHIHHMMMMM"
+ // #        :V:. VIMA:I..  .     .  . .. . .  .:.I:I:..:IHHHHMMHHMMM
+ // #        :"VI:.VWMA::. .:      .   .. .:. ..:.I::.:IVHHHMMMHMMMMI
+ // #        :."VIIHHMMA:.  .   .   .:  .:.. . .:.II:I:AMMMMMMHMMMMMI
+ // #        :..VIHIHMMMI...::.,:.,:!"I:!"I!"I!"V:AI:VAMMMMMMHMMMMMM'
+ // #        ':.:HIHIMHHA:"!!"I.:AXXXVVXXXXXXXA:."HPHIMMMMHHMHMMMMMV
+ // #          V:H:I:MA:W'I :AXXXIXII:IIIISSSSSSXXA.I.VMMMHMHMMMMMM
+ // #            'I::IVA ASSSSXSSSSBBSBMBSSSSSSBBMMMBS.VVMMHIMM'"'
+ // #             I:: VPAIMSSSSSSSSSBSSSMMBSSSBBMMMMXXI:MMHIMMI
+ // #            .I::. "H:XIIXBBMMMMMMMMMMMMMMMMMBXIXXMMPHIIMM'
+ // #            :::I.  ':XSSXXIIIIXSSBMBSSXXXIIIXXSMMAMI:.IMM
+ // #            :::I:.  .VSSSSSISISISSSBII:ISSSSBMMB:MI:..:MM
+ // #            ::.I:.  ':"SSSSSSSISISSXIIXSSSSBMMB:AHI:..MMM.
+ // #            ::.I:. . ..:"BBSSSSSSSSSSSSBBBMMMB:AHHI::.HMMI
+ // #            :..::.  . ..::":BBBBBSSBBBMMMB:MMMMHHII::IHHMI
+ // #            ':.I:... ....:IHHHHHMMMMMMMMMMMMMMMHHIIIIHMMV"
+ // #              "V:. ..:...:.IHHHMMMMMMMMMMMMMMMMHHHMHHMHP'
+ // #               ':. .:::.:.::III::IHHHHMMMMMHMHMMHHHHM"
+ // #                 "::....::.:::..:..::IIIIIHHHHMMMHHMV"
+ // #                   "::.::.. .. .  ...:::IIHHMMMMHMV"
+ // #                     "V::... . .I::IHHMMV"'
+ // #                       '"VHVHHHAHHHHMMV:"'
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log(
     "%c Easy Day",
